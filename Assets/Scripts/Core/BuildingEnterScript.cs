96181316e04b612a6d0ca71c84eb0b2aa76b4845
Liamcodeshard <99624436+Liamcodeshard@users.noreply.github.com>
2023-04-04 17:18:01 +0100

    building Functionality
    Added ability to enter buildings
    1 issue - main light is being kept on by other buildings
    need to create a static? reference to the lights that is triggered - not updated
    Event system?

---
+++ Assets/Scripts/Core/BuildingEnterScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class BuildingEnterScript : MonoBehaviour
+{
+    [SerializeField] GameObject inner, outer, mainLighting;
+    Vector3 centrePoint;
+    GameObject player;
+    [SerializeField] float buildingWidth;
+    void Start()
+    {
+        centrePoint = this.transform.position;
+        OnBuildingExit();
+    }
+
+    void Update()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (IsInRange())
+        {
+            print("Entered");
+            OnBuildingEnter();
+        }
+        else
+        {
+            OnBuildingExit();
+        }
+    }
+
+    private bool IsInRange()
+    {
+        return Vector3.Distance(centrePoint, player.transform.position) < buildingWidth;
+    }
+
+    public void OnBuildingEnter()
+    {
+        inner.SetActive(true);
+        outer.SetActive(false);
+        mainLighting.SetActive(false);
+        print("Entered");
+    }
+    public void OnBuildingExit()
+    {
+
+        inner.SetActive(false);
+        outer.SetActive(true);
+        mainLighting.SetActive(true);
+        print("Exited");
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(this.transform.position, buildingWidth);
+    }
+}
