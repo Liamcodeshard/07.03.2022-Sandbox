8a77470b145d75c55a6ebd21866e4add546c4f47
Liamcodeshard <99624436+Liamcodeshard@users.noreply.github.com>
2023-08-12 03:50:00 +0100

    scene switchinggg bitchesss

---
+++ Assets/Portal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        public string sceneName;
+
+        void OnTriggerEnter(Collider other)
+        {
+            if(other.tag == "Player")
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+
+        }
+    }
+    
+}
